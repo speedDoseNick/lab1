300940836db2e3477b9ccf86c3df8384fc6243cb
speedDoseNick <bychkov_roman@bk.ru>
2024-09-14 19:43:27 +0500

    fourth

--- oopLab1_V2/Date.h
+++ oopLab1_V2/Date.h
@@ -1,4 +1,4 @@
-#ifndef DATE_H
+п»ї#ifndef DATE_H
 #define DATE_H
 
 #include <iostream>
@@ -14,44 +14,44 @@ private:
     void validateDate(int y, int m, int d) const noexcept;
 
 public:
-    // Конструкторы
+    // РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂС‹
     Date();
     Date(int y, int m, int d);
     Date(const Date& other);
 
-    // Методы инициализации
+    // РњРµС‚РѕРґС‹ РёРЅРёС†РёР°Р»РёР·Р°С†РёРё
     void Init(int y, int m, int d);
     void Init(const Date& other);
 
-    // Методы для ввода/вывода
+    // РњРµС‚РѕРґС‹ РґР»СЏ РІРІРѕРґР°/РІС‹РІРѕРґР°
     void Read();
     void Display() const noexcept;
 
-    // Преобразование в строку
+    // РџСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёРµ РІ СЃС‚СЂРѕРєСѓ
     std::string toString() const noexcept;
 
-    // Геттеры
+    // Р“РµС‚С‚РµСЂС‹
     int getYear() const noexcept;
     int getMonth() const noexcept;
     int getDay() const noexcept;
 
-    // Сеттеры
+    // РЎРµС‚С‚РµСЂС‹
     void setYear(int y);
     void setMonth(int m);
     void setDay(int d);
 
-    // Операции с датами
+    // РћРїРµСЂР°С†РёРё СЃ РґР°С‚Р°РјРё
     Date addDays(int days) const noexcept;
     Date subtractDays(int days) const noexcept;
     bool isLeapYear() const noexcept;
 
-    // Сравнение дат
+    // РЎСЂР°РІРЅРµРЅРёРµ РґР°С‚
     bool operator==(const Date& other) const noexcept;
     bool operator!=(const Date& other) const noexcept;
     bool operator<(const Date& other) const noexcept;
     bool operator>(const Date& other) const noexcept;
 
-    // Количество дней между датами
+    // РљРѕР»РёС‡РµСЃС‚РІРѕ РґРЅРµР№ РјРµР¶РґСѓ РґР°С‚Р°РјРё
     int daysBetween(const Date& other) const noexcept;
 };
 
